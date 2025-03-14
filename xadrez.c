f2d4f6e53b3be6d8f9c5bb009d08d36cc8ee8139
Lucas do Nascimento Silva <160559635+LucasNascimento7@users.noreply.github.com>
2025-03-14 02:13:53 +0000

    recursividade

--- xadrez.c
+++ xadrez.c
@@ -4,37 +4,93 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-  
-  for (int i = 0; i < 5; i++)
-  {
-    printf("bispo: cima direita \n");
-  }
-  
-  for (int i = 0; i < 5; i++)
-  {
-    printf("torre: direita \n");
-  }
-  
-  for (int i = 0; i < 8; i++)
-  {
-    printf("rainha: esquerda \n");
-  }
+void moverBispo(int casas)
+{
 
-  int cavalo = 1;
-
-  while (cavalo--)
-  printf("cavalo:");
+  if (casas > 0)
   {
-    for (int i = 0; i < 3; i++)
+
+    int bispo = 5;
+
+    while (bispo--)
+
     {
-      printf("cima ");
-      i++;
+      for (int i = 0; i < casas; i++)
+      {
+        printf("cima ");
+        i++;
+      }
+      printf("direita \n");
     }
-    printf("direita ");
-  }
-  
-  
 
-    return 0;
+    moverBispo(casas - 1);
+  }
+}
+
+void moverTorre(int casas)
+{
+
+  if (casas > 0)
+  {
+
+    moverTorre(casas - 1);
+
+    printf("direita \n");
+  }
+}
+
+void moverRainha(int casas)
+{
+
+  if (casas > 0)
+  {
+
+    moverRainha(casas - 1);
+
+    printf("direita \n");
+  }
+}
+
+void moverCavalo(int casas)
+{
+
+  if (casas > 0)
+  {
+
+    int cavalo = 1;
+
+    while (cavalo--)
+
+    {
+      for (int i = 0; i < 3; i++)
+      {
+        printf("cima ");
+        i++;
+      }
+      printf("direita ");
+    }
+
+    moverCavalo(casas - 1);
+  }
+}
+
+int main()
+{
+  printf("bispo: \n");
+
+  moverBispo(1);
+
+  printf("torre: \n");
+
+  moverTorre(5);
+
+  printf("rainha: \n");
+
+  moverRainha(8);
+
+  printf("cavalo: \n");
+
+  moverCavalo(1);
+
+  return 0;
 }
